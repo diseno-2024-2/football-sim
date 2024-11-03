2305cbbee0b4c152b016e123a4cc41070a6b81b5
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-11-03 13:33:22 +0100

    feat: implement template for ball class

---
+++ srcs/classes/ball.py
@@ -0,0 +1,19 @@
+from utils import Coordinates
+
+
+class Ball():
+    def __init__(self):
+        self.coordinates: Coordinates = Coordinates()
+        self.radius: float = 1  # Change later to apropiate value
+
+    def kick():
+        pass
+
+    def control():
+        pass
+
+    def pass_ball():
+        pass
+
+    def catch():
+        pass
