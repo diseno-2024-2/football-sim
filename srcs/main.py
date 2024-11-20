cd7779c9c725aa72fe570007d659d6409d9c6145
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-11-20 17:19:02 +0100

    feat: connect to mangodb

--- srcs/main.py
+++ srcs/main.py
@@ -1,27 +1,26 @@
-from classes.field import Field
-from classes.player import Player
-from classes.utils import Coordinates
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from time import sleep 
-from classes.screen import Screen
+# from classes.field import Field
+# from classes.player import Player
+# from classes.utils import Coordinates
+# from time import sleep
+# from classes.screen import Screen
+
+import pymongo
+
 
 def main():
-    time = 1
-    field = Field()
-    player = Player("Jugador",1,Coordinates(0,0),field,time)
-    screen = Screen([player],field,960,480)
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["tmp"]
+    print(db)
+    # time = 1
+    # field = Field()
+    # player = Player("Jugador", 1, Coordinates(0, 0), field, time)
+    # screen = Screen([player], field, 960, 480)
 
-    while True:
-        screen.visualice()
-        player.behavior()
-        sleep(0.1)
-        print(player)
-        
-        pass
-   
-        
-   
+    # while True:
+    #     screen.visualice()
+    #     player.behavior()
+    #     sleep(0.1)
+    #     print(player)
 
 
 if __name__ == '__main__':
